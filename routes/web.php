1a46d275e2ee7eec2c5dfea42a901185cec32e99
Bond <kraiwit.dev@gmail.com>
2022-10-17 21:57:16 +0700

    set route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\PaymentController@checkout')->name('checkout');
+Route::post('/session', 'App\Http\Controllers\PaymentController@session')->name('session');
+Route::get('/success', 'App\Http\Controllers\PaymentController@success')->name('success');
